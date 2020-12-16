871bae079c1bdd8ff3e4e8d4dde337b86f316273
Tiffany Wong <wongct@uw.edu>
2020-12-16 12:51:54 -0800

    add firebase

--- fresh-take/src/index.js
+++ fresh-take/src/index.js
@@ -1,5 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import firebase from 'firebase/app';
+
+// The core Firebase JS SDK is always required and must be listed first 
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC0sp2-p_gS_S16Tqksc_GrsmdkndWkprA",
+    authDomain: "info442-fresh-take.firebaseapp.com",
+    projectId: "info442-fresh-take",
+    storageBucket: "info442-fresh-take.appspot.com",
+    messagingSenderId: "218385463861",
+    appId: "1:218385463861:web:0821a30bcf0e8fbdee8f7c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
